b13312a74c1f0939e15150fa819ce32b874df0d5
Joseg-05 <49496262+Joseg-05@users.noreply.github.com>
2020-11-09 14:56:56 -0800

    Enemy Follow the Player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,18 +3,22 @@ using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     private MeshRenderer render;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        
         render = gameObject.GetComponent<MeshRenderer>();
         render.material.SetColor("_Color", Color.red);
     }
 
+
+   
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Trash : MonoBehaviour
+{
+
+    private NavMeshAgent Mob;
+
+    public GameObject Coco;
+
+    public float MobDistanceRun = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mob = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Coco.transform.position);
+
+        if(distance < MobDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Coco.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Mob.SetDestination(newPos);
+
+        }
+    }
+}
